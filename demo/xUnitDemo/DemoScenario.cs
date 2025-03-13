abed80c376d220b8c64da3ea471b5f6baeaf4c92
Vasyl Vdovychenko <mrviduus@gmail.com>
2025-03-13 16:31:22 -0400

    Add xUnitDemo project with initial load test scenario and update solution

---
+++ demo/xUnitDemo/DemoScenario.cs
@@ -0,0 +1,13 @@
+﻿using xUnitLoadFramework;
+
+namespace xUnitDemo;
+
+public class DemoScenario
+{
+    [Fact]
+    [LoadTestSettings(concurrency:10, DurationInSeconds = 5, IntervalInSeconds = 1)]
+    public void Test1()
+    {
+        Console.WriteLine("This is a very fast test");
+    }
+}
