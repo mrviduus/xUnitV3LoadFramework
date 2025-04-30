d2d167d7587de4c130407d6e0d65f6597433d59b
Vasyl Vdovychenko <mrviduus@gmail.com>
2025-04-30 15:29:03 -0400

    update

--- tests/xUnitLoadRunnerTests/ExampleTestFramework.cs
+++
@@ -1,8 +0,0 @@
-// using Xunit.Abstractions;
-// using xUnitLoadFramework;
-//
-// [assembly:Xunit.TestFramework("xUnitLoadRunnerTests.ExampleTestFramework", "xUnitLoadRunnerTests")]
-//
-// namespace xUnitLoadRunnerTests;
-//
-// public class ExampleTestFramework(IMessageSink messageSink) : LoadTestFramework(messageSink);
